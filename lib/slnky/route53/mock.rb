8d2079fed45225794bca59410e25dfcdd80ffbe0
Shawn Catanzarite <me@shawncatz.com>
2016-04-21 11:19:14 -0700

    add initializer to mock to avoid creating fog objects

--- lib/slnky/route53/mock.rb
+++ lib/slnky/route53/mock.rb
@@ -3,9 +3,9 @@ module Slnky
     class Mock < Slnky::Route53::Client
       # unless there's something special you need to do in the initializer
       # use the one provided by the actual client object
-      # def initialize
-      #
-      # end
+      def initialize
+
+      end
 
       def remove_records(id)
         if id == 'i-12345678'
