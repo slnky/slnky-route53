52964caabc77d0b8e11a78e58fb35c9477ad8149
Shawn Catanzarite <me@shawncatz.com>
2016-04-22 12:14:41 -0700

    this one has an additional command, so test shouldn't have changed

--- spec/slnky/route53/command_spec.rb
+++ spec/slnky/route53/command_spec.rb
@@ -20,6 +20,6 @@ describe Slnky::Route53::Command do
 
   it 'handles help' do
     expect { subject.handle(help.name, help.payload, help_response) }.to_not raise_error
-    expect(help_response.trace).to include("route53 has no additional commands")
+    expect(help_response.trace).to include("route53 help: print help")
   end
 end
