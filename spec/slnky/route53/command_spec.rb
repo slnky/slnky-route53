30d29132ae13945e4c9d412a7f0caa9ca481cdef
Shawn Catanzarite <me@shawncatz.com>
2016-04-21 19:37:45 -0700

    commented out echo code, now the test, duh

--- spec/slnky/route53/command_spec.rb
+++ spec/slnky/route53/command_spec.rb
@@ -11,12 +11,12 @@ describe Slnky::Route53::Command do
   let(:echo_response) { slnky_response('test-route', 'spec') }
   let(:help_response) { slnky_response('test-route', 'spec') }
 
-  it 'handles echo' do
-    # make sure the command handler does not raise an error
-    expect { subject.handle(echo.name, echo.payload, echo_response) }.to_not raise_error
-    # validate that the correct output is available in the response object
-    expect(echo_response.trace).to include("test echo")
-  end
+  # it 'handles echo' do
+  #   # make sure the command handler does not raise an error
+  #   expect { subject.handle(echo.name, echo.payload, echo_response) }.to_not raise_error
+  #   # validate that the correct output is available in the response object
+  #   expect(echo_response.trace).to include("test echo")
+  # end
 
   it 'handles help' do
     expect { subject.handle(help.name, help.payload, help_response) }.to_not raise_error
